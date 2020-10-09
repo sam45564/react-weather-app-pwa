50b2f23999cbed1e2c765494e57b9b4cfccc9e43
Samsul Hoque Choudhury <sam45564@gmail.com>
2020-10-09 12:18:44 +0530

    Added UI elements and service to fetch and display the data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,27 +6,55 @@ import { fetchWeather } from "../services/fetchWeather";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
   const search = async (event) => {
     if (event.key === "Enter") {
-      const data = fetchWeather(query);
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery('');
     }
   };
 
   return (
     <div className="container vertical-center">
-      <div className="row">
-        <div className="col-6 offset-4">
-          <input type="text" className="form-control" placeholder="Enter City..." value={query} onChange={(event) => setQuery(event.target.value)} />
-
-          <div className="card mt-3">
-            <div className="card-body">
-              <h5 className="card-title">{query}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div className="row col-12">
+        <div className="col-6 offset-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter City..."
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            onKeyPress={search}
+          />
+        </div>
+        {weather.main && (
+          <div className="col-6 offset-3 mt-2 city">
+            <div className="card">
+              <div className="card-body  text-center">
+                <h3 className="card-title city-name">
+                  <span>{weather.name}</span>
+                  <sup>{weather.sys.country}</sup>
+                </h3>
+                <div className="card-text">
+                  <div className="city-temp">
+                    {Math.round(weather.main.temp)}
+                    <sup>&deg;C</sup>
+                  </div>
+                  <div className="info">
+                    <img
+                      className="city-icon"
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt={weather.weather[0].description}
+                    />
+                    <p>{weather.weather[0].description}</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
